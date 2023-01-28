a3458d2220b0dfc6c7c5b4e5ae3e668dfcaded8c
a-epifanov <a-epifanov87@yandex.ru>
2023-01-29 00:27:09 +0300

    - Начало: Конец: Шаг

--- string.py
+++ string.py
@@ -4,3 +4,4 @@ print(f'{b} {2} {3} TEST')
 my_string = 'test string'
 print(my_string.capitalize())  # С заглавной буквы
 print(my_string.split(' '))  # Разложить строку в массив подстрок
+print(my_string[1:5:1]) # - Начало: Конец: Шаг
