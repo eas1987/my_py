249cb80c1e47beb6bf74004c7fbf96acb45d9b6f
a-epifanov <a-epifanov87@yandex.ru>
2023-01-29 00:13:40 +0300

    split

--- string.py
+++ string.py
@@ -2,5 +2,5 @@ b = 'test'
 print(f'{b} {2} {3} TEST')
 
 my_string = 'test string'
-print(my_string.capitalize()) # С заглавной буквы
-print(my_string.split(' ')) # Разложить строку в массив подстрок
+print(my_string.capitalize())  # С заглавной буквы
+print(my_string.split(' '))  # Разложить строку в массив подстрок
