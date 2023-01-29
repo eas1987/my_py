9b6da93ffb2c55ab8f61ee1c12c47abeb216e853
a-epifanov <a-epifanov87@yandex.ru>
2023-01-29 23:24:08 +0300

    func

---
+++ function_функции.py
@@ -0,0 +1,8 @@
+def test(a, b):
+    # print(word)
+    return a + b
+
+
+print(test(1, 3))
+
+
--- set_множества.py
+++ set_множества.py
@@ -8,8 +8,8 @@ my_set.add(2)
 # my_set.pop()
 # my_set.clear()
 
-
-print(my_set)
+my_set_2 = set([1, 2, 3, 4])
+print(my_set.union(my_set_2))
 
 new_fr_srt = frozenset([1, 2, 3, 4])
 print(new_fr_srt)
