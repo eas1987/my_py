f00c55f0638b998eba92bf57519360e461cf991f
a-epifanov <a-epifanov87@yandex.ru>
2023-02-01 23:33:46 +0300

    111

--- test.py
+++ test.py
@@ -1,12 +1,42 @@
-count = int(input('кол - во элементов '))
-new_list = []
-for i in range(1, count + 1):
-    # in_val = 'новый элемент ' + str(i) + ': '
-    # el = input(in_val)
-    el = input(f'new el # {i}: ')
-    new_list.append(el)
+import math
 
-print(new_list)
 
-b = 'test'
-print(f'{b} {2} {3} TEST')
+
+a = 1
+b = a
+b = 2
+
+print(a, b)
+
+
+
+A = {'a', 'c', 'd'}
+B = {'c', 'd', 2 }
+C = {1, 2, 3}
+print('A U B =', A.union(B))
+print('B U C =', B.union(C))
+print('A U B U C =', A.union(B, C))
+print('A.union() =', A.union())
+
+
+
+my_list = [1, 2, 3, 4]
+my_set = {1, 1, 2, 3}
+
+print(my_set)
+
+print(len('test'))
+
+
+# count = int(input('кол - во элементов '))
+# new_list = []
+# for i in range(1, count + 1):
+#     # in_val = 'новый элемент ' + str(i) + ': '
+#     # el = input(in_val)
+#     el = input(f'new el # {i}: ')
+#     new_list.append(el)
+#
+#
+#
+# b = 'test'
+# print(f'{b} {2} {3} TEST')
