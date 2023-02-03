6d092463b1f2eb77578d5ff530e3291a4fcf144e
a-epifanov <a-epifanov87@yandex.ru>
2023-02-03 08:52:00 +0300

    map test

--- 3_input_output_map.py
+++ 3_input_output_map.py
@@ -2,3 +2,8 @@ print('2', 2, True, sep='|', end='-')
 
 x, y = map(str, input('ввод нескольких значений через пробел ').split(sep=' '))
 print(x, y)
+
+
+numbers = [10, 15, 21, 33, 42, 55]
+mapped_numbers = list(map(lambda elem: elem * 2 + 3, numbers))
+print(mapped_numbers)
--- test1.py
+++ test1.py
@@ -1,3 +1,3 @@
-a, b = map(str, input().split(' '))
-print(f"Коды: {a} = {ord(a)}, {b} = {ord(b)}")
-
+numbers = [10, 15, 21, 33, 42, 55]
+mapped_numbers = list(map(lambda x: x * 2 + 3, numbers))
+print(mapped_numbers)
