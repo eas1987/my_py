d8d1f957c5093e338fa0de574629a361613a8086
a-epifanov <a-epifanov87@yandex.ru>
2023-02-04 00:46:43 +0300

    f - строки, r - строки

--- test1.py
+++ test1.py
@@ -1,2 +1,4 @@
-myStr = input()
-print(myStr.replace(' ', ';'))
+exchange_rate = float(input())
+sum_r = int(input())
+
+print(f'Вы можете получить {int(sum_r // exchange_rate)}$ за {sum_r} рублей по курсу {exchange_rate}')
