083f1f31d90769647af1b02a0ec457b361af7f13
a-epifanov <a-epifanov87@yandex.ru>
2023-02-27 23:08:08 +0300

    Циклы

---
+++ 6_циклы.py
@@ -0,0 +1,9 @@
+
+lst = list('test')
+for index, value in enumerate(lst):             #Перебор пары "Индекс, Значение"
+    print(index, value)
+
+
+# it = iter(lst)
+# while next(it):
+#     print(it)
